27405da2196fb0705bf5297d9d151c83bd2166bf
melintemarian <48149829+melintemarian@users.noreply.github.com>
2019-05-29 21:07:34 +0300

    Modificari
    Am scos Exceptiile si datele pt ca dadeau erori.

--- Functii Proceduri Simple.sql
+++ Functii Proceduri Simple.sql
@@ -9,21 +9,16 @@ is
             if(statia_sosire=NULL or statia_sosire=NULL) then
             raise input_inexistent;
             end if;
-            Open cursorul for select st.nume_statie,ss.NUME_STATIE,tr.ORA_PLECARE-floor(tr.ORA_PLECARE)+data,tr.ORA_SOSIRE-floor(tr.ORA_SOSIRE)+data,ID_TRASEU from statii st
+            Open cursorul for select st.nume_statie,ss.NUME_STATIE,tr.ORA_PLECARE,tr.ORA_SOSIRE,ID_TRASEU from statii st
             join TRASEU tr on st.ID_STATIE=tr.ID_STATIE_PLECARE
             join statii ss on tr.ID_STATIE_SOSIRE=ss.ID_STATIE
             where st.NUME_STATIE=statia_plecare and ss.Nume_statie=statia_sosire;
-            EXCEPTION
-            WHEN no_data_found THEN
-            DBMS_OUTPUT.PUT_LINE('Niciun rezultat gasit.');
-            WHEN input_inexistent THEN
-            DBMS_OUTPUT.PUT_LINE('Niciun rezultat gasit.');
             return cursorul;
         end;
     /
 
 drop function  Cautare_Statii_Plecare;
-create or replace function Cautare_Statii_Plecare(denumire_statie in varchar2,data date)
+create or replace function Cautare_Statii_Plecare(denumire_statie in varchar2)
 return sys_refcursor
 is
     cursorul sys_refcursor;
@@ -34,15 +29,10 @@ is
             raise input_inexistent;
             end if;
             Open cursorul for
-                select st.NUME_STATIE,ss.NUME_STATIE,ORA_PLECARE-floor(ORA_PLECARE)+data,ID_TRASEU from statii st
+                select st.NUME_STATIE,ss.NUME_STATIE,ORA_PLECARE,ID_TRASEU from statii st
                 join traseu ts on ts.ID_STATIE_PLECARE=st.ID_STATIE
                 join statii ss on ss.ID_STATIE=ts.ID_STATIE_SOSIRE
                 where st.NUME_STATIE=trim(denumire_statie);
-            EXCEPTION
-            WHEN no_data_found THEN
-            DBMS_OUTPUT.PUT_LINE('Niciun rezultat gasit.');
-            WHEN input_inexistent then
-            DBMS_OUTPUT.PUT_LINE('Niciun input gasit.');
             return cursorul;
         end;
     /
@@ -59,15 +49,10 @@ is
             raise input_inexistent;
             end if;
             Open cursorul for
-                select ss.NUME_STATIE,st.NUME_STATIE,ORA_SOSIRE-floor(ORA_SOSIRE)+data,ID_TRASEU from statii st
+                select ss.NUME_STATIE,st.NUME_STATIE,ORA_SOSIRE,ID_TRASEU from statii st
                 join traseu ts on ts.ID_STATIE_SOSIRE=st.ID_STATIE
                 join statii ss on ss.ID_STATIE=ts.ID_STATIE_PLECARE
                 where st.NUME_STATIE=trim(denumire_statie);
-            EXCEPTION
-            WHEN no_data_found THEN
-            DBMS_OUTPUT.PUT_LINE('Niciun rezultat gasit.');
-            WHEN input_inexistent then
-            DBMS_OUTPUT.PUT_LINE('Niciun input gasit.');
             return cursorul;
         end;
     /
@@ -88,11 +73,6 @@ is
                 join statii st1 on st1.ID_STATIE=tr.ID_STATIE_PLECARE
                 join statii st2 on st2.ID_STATIE=tr.ID_STATIE_SOSIRE
                 where ID_TRASEU=id_traseu_cautat;
-            EXCEPTION
-            WHEN no_data_found THEN
-            DBMS_OUTPUT.PUT_LINE('Niciun rezultat gasit.');
-            WHEN input_inexistent then
-            DBMS_OUTPUT.PUT_LINE('Niciun input gasit.');
             return cursorul;
         end;
 /
@@ -113,11 +93,6 @@ is
                 join LOCOMOTIVA L2 on M2.ID_LOCOMOTIVA = L2.ID_LOCOMOTIVA
                 join STATII S2 on L2.ID_STATIE_DOMICILIU = S2.ID_STATIE
                 where M2.ID_LOCOMOTIVA=id_locomotiva_s;
-            EXCEPTION
-            WHEN no_data_found THEN
-            DBMS_OUTPUT.PUT_LINE('Niciun rezultat gasit.');
-            WHEN input_inexistent then
-            DBMS_OUTPUT.PUT_LINE('Niciun input gasit.');
             return cursorul;
         end;
     /
@@ -138,7 +113,7 @@ begin
 end;
 /
 
-Drop procedure  Cautare_reparare_statie;
+Drop function  Cautare_reparare_statie;
 Create or replace function Cautare_reparare_statie(id_statie_s int)
 return sys_refcursor
 is
@@ -153,11 +128,6 @@ begin
             select NUME_STATIE,ID_REPARARE,mt.ID_LOCOMOTIVA,DATA_ADAUGARE,DATA_ELIBERARE from MENTENANTA mt
             join statii st on st.ID_STATIE=mt.ID_STATIE
             where st.NUME_STATIE=id_statie_s;
-    EXCEPTION
-    WHEN no_data_found THEN
-            DBMS_OUTPUT.PUT_LINE('Niciun rezultat gasit.');
-    WHEN input_inexistent then
-            DBMS_OUTPUT.PUT_LINE('Niciun input gasit.');
             return cursorul;
 
 end;
